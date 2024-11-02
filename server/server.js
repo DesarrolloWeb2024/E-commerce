494c40f690649c6551401773812eb595d356e0fd
Iripontiroli98 <iripontiroli@gmail.com>
2024-11-02 14:52:47 -0300

    Cambio imagen del producto

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/api/products', (req, res) => {
       name: 'Mouse Logitech', 
       oldPrice: 37500, 
       price: 35000, 
-      urlImg: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSmYckEMeFKNyPfp2TJu1Fup4HIBjibtiCqO3I6nbhWev7LIyxevl52_-aZwkaoJmuOVxzl0GTcx9nqq5G4T3ArSy99H_T5&usqp=CAE'
+      urlImg: 'https://i.ebayimg.com/images/g/UosAAOSwTV5mRyCj/s-l400.png'
     },
     {
       id: 2, 
