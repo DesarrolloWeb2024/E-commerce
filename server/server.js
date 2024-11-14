40f93dc266c976f05e78cb76e5f7eaf3230a11d6
victoriaconte7 <1vickyconte@gmail>
2024-11-14 19:40:41 -0300

    Cambie las imagenes de los productos y los precios, el titulo y quienes somos

--- server/server.js
+++ server/server.js
@@ -12,31 +12,31 @@ app.get('/api/products', (req, res) => {
   const products = [
     { 
       id: 1, 
-      name: 'Cámara web HD 1080P', 
-      oldPrice: 37500, 
-      price: 35000, 
-      urlImg: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSmYckEMeFKNyPfp2TJu1Fup4HIBjibtiCqO3I6nbhWev7LIyxevl52_-aZwkaoJmuOVxzl0GTcx9nqq5G4T3ArSy99H_T5&usqp=CAE'
+      name: 'Matambre', 
+      oldPrice: 7500, 
+      price: 5000, 
+      urlImg: 'https://raffe.com.ar/wp-content/uploads/2019/06/Matambre-Raffe.jpg'
     },
     {
       id: 2, 
-      name: 'Campera Keyboard programador',  
+      name: 'Tapa de asado',  
       oldPrice: 11500, 
       price: 11200, 
-      urlImg: 'https://purodiseno.lat/wp-content/uploads/2023/04/CAMPERA-TECLADO-2-821x1024.jpg' 
+      urlImg: 'https://raffe.com.ar/wp-content/uploads/2018/08/Asado-Raffe.jpg' 
     },
     { 
       id: 3, 
-      name: 'Disco-solido interno kingston sata-960g',  
-      oldPrice: 18500, 
-      price: 18000, 
-      urlImg: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQwqHX1fVP2DUn6mEw05ukydZ6KOgkf0w3N3ZEHHJnDUW7WtxSEzdOZ73YxCn-FBC5kYxGeF4wy9zakpXVDOXbjQouNAmyH1LTbzkyuK2t0u9We1eAqfkFFDw&usqp=CAE' 
+      name: 'Bola de lomo',  
+      oldPrice: 8500, 
+      price: 7000, 
+      urlImg: 'https://raffe.com.ar/wp-content/uploads/2017/06/Bola-de-Lomo-Raffe.jpg' 
     },
     { 
       id: 3, 
-      name: 'Taza Código De Programacion',  
+      name: 'Paleta',  
       oldPrice: 3500, 
       price: 2200, 
-      urlImg: 'https://http2.mlstatic.com/D_NQ_NP_786656-MLA46832914211_072021-O.webp' 
+      urlImg: 'https://raffe.com.ar/wp-content/uploads/2017/06/Paleta-Cuadril-Raffe.jpg' 
     },
     
     
