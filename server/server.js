666e1cce39de107a3845ed6942728afaf9adf68c
Paloma Quiroga <quirogapaloma10@gmail.com>
2024-11-15 10:45:21 -0300

    agregue dos fotos de productos

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get('/api/products', (req, res) => {
       name: 'Campera Keyboard programador',  
       oldPrice: 11500, 
       price: 11200, 
-      urlImg: 'https://purodiseno.lat/wp-content/uploads/2023/04/CAMPERA-TECLADO-2-821x1024.jpg' 
+      urlImg: 'https://cocinerosargentinos.com/content/recipes/500x500/recipes.12842.jpeg' 
     },
     { 
       id: 3, 
@@ -36,7 +36,7 @@ app.get('/api/products', (req, res) => {
       name: 'Taza Código De Programacion',  
       oldPrice: 3500, 
       price: 2200, 
-      urlImg: 'https://http2.mlstatic.com/D_NQ_NP_786656-MLA46832914211_072021-O.webp' 
+      urlImg: 'https://panaderialucca.com.ar/wp-content/uploads/2023/08/productos-panaderia.jpg' 
     },
     
     
