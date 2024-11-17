470a646e11fc658d8db1e6d4bb48b08238bd5447
gonzfloan2023@gmail.com <gonzfloan@gmail.com>
2024-11-17 16:17:57 -0300

    Se agregaron 4 productos con precio y descuento

--- server/server.js
+++ server/server.js
@@ -32,13 +32,41 @@ app.get('/api/products', (req, res) => {
       urlImg: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQwqHX1fVP2DUn6mEw05ukydZ6KOgkf0w3N3ZEHHJnDUW7WtxSEzdOZ73YxCn-FBC5kYxGeF4wy9zakpXVDOXbjQouNAmyH1LTbzkyuK2t0u9We1eAqfkFFDw&usqp=CAE' 
     },
     { 
-      id: 3, 
+      id: 4, 
       name: 'Taza Código De Programacion',  
       oldPrice: 3500, 
       price: 2200, 
       urlImg: 'https://http2.mlstatic.com/D_NQ_NP_786656-MLA46832914211_072021-O.webp' 
     },
-    
+    {
+      id: 5, 
+      name: 'Aceite De Coco Neutro Natural 800ml',  
+      oldPrice: 19600, 
+      price: 18600, 
+      urlImg: 'https://acdn.mitiendanube.com/stores/626/963/products/02-coco-neutro-8001-77d628da6c59fc42ac16916810631318-640-0.webp' 
+    },
+    {
+      id: 6, 
+      name: 'Budin integral Ciruela y Nuez S/Azucar 300g',  
+      oldPrice: 4200, 
+      price: 3700, 
+      urlImg: 'https://acdn.mitiendanube.com/stores/002/133/760/products/budinc211-53d82703ac32cb42c216600979462368-640-0.webp' 
+    },
+    {
+      id: 7, 
+      name: 'Mantequilla de Mani BEEPURE 400g',  
+      oldPrice: 4000, 
+      price: 3550, 
+      urlImg: 'https://acdn.mitiendanube.com/stores/001/130/674/products/beepure_mantequillas-de-mani-370g1-04914bc2fb19ca76c416268819865808-1024-1024.jpg' 
+    },
+    {
+      id: 8,
+      name: 'Harina de Almentras x200g Natural Seed',  
+      oldPrice: 6300, 
+      price: 5680, 
+      urlImg: 'https://latiendadelceliaco.com.ar/wp-content/uploads/2021/04/HARINA-DE-ALMENDRAS-NATURAL-SEED.jpg' 
+    },
+
     
   ];
 
