4e44d4a159bacf0ba74734a5fe236ef8f6459e5c
Brian Nicolas Medina Cari <123790198+nicomedina88@users.noreply.github.com>
2024-11-16 21:18:34 -0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,5 @@
+// public/scripts/main.js
+
 document.addEventListener('DOMContentLoaded', () => {
     // Hacer una solicitud GET al servidor para obtener la información de productos
     try {
@@ -31,9 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
                                                     $${product.price}
                                                     </span>
                                                 </div>
-                                                <div class="col-12 mb-3 align-self-end">
-                                                    <button class="btn btn-outline-dark add-to-cart" data-name="${product.name}" data-price="${product.price}" data-image="${product.urlImg}"><i class="fas fa-cart-plus me-2"></i>Agregar al carrito</button>
-                                                </div>
                                             </div>
                                         </div>
                                     `;
@@ -43,41 +42,39 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => {
             console.error('Error fetching products:', error);
         });
-    } catch (e) {
-        console.log('Error en el try-catch')
+    }catch (e) {
+        console.log('try-catch')
         console.log(e)
     }
-
-    // Manejar la adición de productos al carrito
-    document.getElementById('product-list').addEventListener('click', function(event) {
-        if (event.target && event.target.matches('.add-to-cart')) {
-            // Obtener los datos del producto (nombre, precio, imagen)
-            const productName = event.target.dataset.name;
-            const productPrice = event.target.dataset.price;
-            const productImage = event.target.dataset.image;
-
-            // Crear el elemento del carrito
-            const cartItem = `
-                <div class="cart-item d-flex justify-content-between mb-2">
-                    <img src="${productImage}" alt="${productName}" class="cart-item-image" style="width: 50px;">
-                    <div class="cart-item-info d-flex flex-column justify-content-center">
-                        <span>${productName}</span>
-                        <span>$${productPrice}</span>
-                    </div>
-                    <button class="btn btn-danger btn-sm remove-item" onclick="removeItem(this)">X</button>
-                </div>
-            `;
-
-            // Agregar el producto al carrito
-            document.getElementById("cart-items").insertAdjacentHTML('beforeend', cartItem);
-
-            // Mostrar el carrito de compras (modal)
-            $("#shopping-cart").modal("show");
-        }
-    });
 });
+  
 
-// Función para eliminar un producto del carrito
-function removeItem(button) {
-    button.closest(".cart-item").remove();
-}
+/***
+ *   
+                                    <div class="col-lg-3 col-sm-6 my-3">
+                                        <div class="col-12 bg-white text-center h-100 product-item">
+                                            <div class="row h-100">
+                                                <div class="col-12 p-0 mb-3">
+                                                    <a href="#">
+                                                        <img src="images/image-1.jpg" class="img-fluid">
+                                                    </a>
+                                                </div>
+                                                <div class="col-12 mb-3">
+                                                    <a href="#" class="product-name">Sony Alpha DSLR Camera</a>
+                                                </div>
+                                                <div class="col-12 mb-3">
+                                                    <span class="product-price-old">
+                                                        $500
+                                                    </span>
+                                                    <br>
+                                                    <span class="product-price">
+                                                        $500
+                                                    </span>
+                                                </div>
+                                                <div class="col-12 mb-3 align-self-end">
+                                                    <button class="btn btn-outline-dark" type="button"><i class="fas fa-cart-plus me-2"></i>Add to cart</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+ */
