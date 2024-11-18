76b3266416d9acb6193adf5f6b4ddedc4d85bce6
Paloma Quiroga <quirogapaloma10@gmail.com>
2024-11-18 12:24:14 -0300

    agregue fotos a productos

--- server/server.js
+++ server/server.js
@@ -15,28 +15,28 @@ app.get('/api/products', (req, res) => {
       name: 'Pan frances', 
       oldPrice: 2500, 
       price: 2000, 
-      urlImg: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSmYckEMeFKNyPfp2TJu1Fup4HIBjibtiCqO3I6nbhWev7LIyxevl52_-aZwkaoJmuOVxzl0GTcx9nqq5G4T3ArSy99H_T5&usqp=CAE'
+      urlImg: 'https://enrilemoine.com/wp-content/uploads/2024/06/pan-frances-by-enrilemoine-13.webp'
     },
     {
       id: 2, 
       name: 'medialunas de manteca',  
       oldPrice: 4500, 
       price: 4000, 
-      urlImg: 'https://purodiseno.lat/wp-content/uploads/2023/04/CAMPERA-TECLADO-2-821x1024.jpg' 
+      urlImg: 'https://www.midiariodecocina.com/wp-content/uploads/2022/01/Medialunas03.jpg' 
     },
     { 
       id: 3, 
-      name: 'medialunas de grasa',  
-      oldPrice: 4500, 
-      price: 4000, 
-      urlImg: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQwqHX1fVP2DUn6mEw05ukydZ6KOgkf0w3N3ZEHHJnDUW7WtxSEzdOZ73YxCn-FBC5kYxGeF4wy9zakpXVDOXbjQouNAmyH1LTbzkyuK2t0u9We1eAqfkFFDw&usqp=CAE' 
+      name: 'Pan lactal',  
+      oldPrice: 5500, 
+      price: 5000, 
+      urlImg: 'https://cuk-it.com/wp-content/uploads/2023/01/pandemolde-stories-4.webp' 
     },
     { 
       id: 3, 
       name: 'rosca de pascua',  
       oldPrice: 5000, 
       price: 4000, 
-      urlImg: 'https://www.rionegro.com.ar/wp-content/uploads/2021/03/165257536_838201626767443_190214730888920895_n.jpg' 
+      urlImg: 'https://cocinerosargentinos.com/content/recipes/original/recipes.19510.jpg' 
     },
     
     
